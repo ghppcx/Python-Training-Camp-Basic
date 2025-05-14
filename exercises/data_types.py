94e68a2081fc23429856a46633b477670a33a8c7
ghppcx <2563110552@qq.com>
2025-05-14 23:42:13 +0800

    完成 data_types.py 练习

--- exercises/data_types.py
+++ exercises/data_types.py
@@ -22,4 +22,10 @@ def get_data_types():
     # 3. 创建一个字符串变量，值为 "Python编程"
     # 4. 创建一个布尔值变量，值为 True
     # 5. 将这些变量作为元组返回
+    integer = 42
+    float = 3.14
+    string = "Python编程"
+    boolean = True
+    return (integer, float, string, boolean)
+
     pass 
