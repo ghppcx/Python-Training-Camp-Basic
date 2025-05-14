353ec2d21b7c412a3b541787aee1126be7d1d153
ghppcx <2563110552@qq.com>
2025-05-15 00:01:39 +0800

    完成 function_params.py 练习

--- exercises/function_params.py
+++ exercises/function_params.py
@@ -21,4 +21,9 @@ def calculate_area(length, width=None):
     - 计算得到的面积
     """
     # 请在下方编写代码
+    if width is None:
+        area = length * length
+    else:
+        area = length * width
+    return area
     pass 
